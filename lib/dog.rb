f1f7f12d3606163d6ba66b13d90fce8fccb8b8af
J. Singleton <jennifer.singleton@gmail.com>
2019-03-17 18:59:24 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -42,8 +42,12 @@ class Dog
     dog
   end
 
-  def new_from_db
+  def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    breed = row[2]
 
+    self.new(id:id, name:name, breed:breed)
   end
 
   def self.find_by_id(id)
@@ -53,6 +57,7 @@ class Dog
       WHERE id = ?
     SQL
 
-    DB[:conn].execute(sql, id)
+    row = DB[:conn].execute(sql, id)
+    new_from_db(row)
   end
 end
