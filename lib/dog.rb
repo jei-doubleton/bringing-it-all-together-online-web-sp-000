029eb056ce7cacbbb7cf5515122fd5bd6ab1850a
J. Singleton <jennifer.singleton@gmail.com>
2019-03-17 19:10:24 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -80,9 +80,9 @@ class Dog
       FROM dogs
       WHERE name = ? AND breed = ?
     SQL
-
+    
     dog = DB[:conn].execute(sql, name, breed)
-
+    
     if !dog.empty?
       dog_info = dog[0]
       dog = Dog.new(id:dog_info[0], name:dog_info[1], breed:dog_info[2])
