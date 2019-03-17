12ca7bc786254b3d504a7093174d1c7c66e55086
J. Singleton <jennifer.singleton@gmail.com>
2019-03-17 19:10:34 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -87,7 +87,7 @@ class Dog
       dog_info = dog[0]
       dog = Dog.new(id:dog_info[0], name:dog_info[1], breed:dog_info[2])
     else
-      dog = self.create(name: name, album:album)
+      dog = self.create(name:name, breed:breed)
     end
     dog
   end
